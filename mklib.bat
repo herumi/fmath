eaf2121dcdb78acbd215a746c431fb59e88f0365
MITSUNARI Shigeo <herumi@nifty.com>
2023-08-22 15:19:04 +0900

    add debug info option

--- mklib.bat
+++ mklib.bat
@@ -1,4 +1,4 @@
 @echo off
 python3 src/gen_fmath.py -m masm > src/fmath.asm
-ml64 /nologo /c /Foobj/fmath.obj src/fmath.asm
+ml64 /nologo /Zd /Zi /c /Foobj/fmath.obj src/fmath.asm
 lib /nologo /OUT:lib/fmath.lib /nodefaultlib obj/fmath.obj
