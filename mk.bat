e760bb544c65e22da6710897f3a897e7f807ac3c
MITSUNARI Shigeo <herumi@nifty.com>
2025-01-10 13:33:11 +0900

    put output exe in bin/

--- mk.bat
+++ mk.bat
@@ -1,3 +1,8 @@
 @echo off
-set OPT=/EHsc -I include -I test /W4 /Oy /O2 /arch:AVX2 /DNDEBUG /DNOMINMAX lib/fmath.lib
-cl %OPT% %1%
+set SRC=%1
+set EXE=%SRC:.cpp=.exe%
+set EXE=%EXE:.c=.exe%
+set EXE=%EXE:test\=bin\%
+echo CCC %EXE%
+set OPT=/nologo /EHsc -I include -I test /W4 /Oy /O2 /arch:AVX2 /std:c++20 /DNDEBUG /DNOMINMAX lib/fmath.lib
+cl %OPT% %SRC% /Fe:%EXE%
